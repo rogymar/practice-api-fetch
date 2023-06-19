4dc9bd157782b1c3b9e87c529f12df14cd3bdddf
rogymar <rogymarchirinos@gmail.com>
2023-06-18 21:41:24 -0300

    Se agregaro funciones que permiten traer los avatares del total de usuarios que existen en la data, así como botones para ir de una página a otra, cambiando con ello tanto el porcentaje como los avatares que se muestran según la página en la que se encuentra. Se relizó un cambio en el services para incluir la paginación, por lo que se construyó la query necesaria.

--- components/User.jsx
+++ components/User.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 
-import { getUser } from '../services/users';
+import { getUser } from "@componentes/services/users";
 import { getLongNameUsers } from "@componentes/util/userUtils";
 
 const getLongNamesPorcentage = (users) => {
@@ -12,16 +12,46 @@ const getLongNamesPorcentage = (users) => {
     return percentage.toFixed(2)
 }
 
+const renderImage = (users) => {
+    return users.map((user) => (
+        <div className=''>
+            <img src={user.avatar} alt="not found avatar" />
+        </div>
+    ))
+}
+
 export default function User () {
     const [users, setUsers] = useState(false)
+    const [page, setPage] = useState(1)
+    const [totalPages, setTotalPages] = useState(1)
 
-    !users && getUser()
-        .then((obj) => setUsers(obj.data))
+    const prevPage = () => {
+        setPage(page - 1)
+        setUsers(null)
+    }
+
+    const nextPage = () => {
+        setPage(page + 1)
+        setUsers(null)
+    }
+
+    !users && getUser(page)
+        .then((obj) => {
+            setUsers(obj.data)
+            setTotalPages(obj.total_pages)
+        })
         .catch((e) => console.error(`${e.name}: ${e.message}`))
 
     return (
         <div>
+            <div>
+                <button onClick={prevPage} className='border border-purple-500 p-2' disabled={page <= 1}>Prev...</button>
+                <span className='mx-2'>Page {page} of {totalPages}</span>
+                <button onClick={nextPage} className='border border-purple-500 p-2' disabled={page >= totalPages}>Next...</button>
+            </div>
             <h2>{users ? `Long name porcentage: ${getLongNamesPorcentage(users)}%` : 'Waiting for users...'}</h2>
+            <h2 className='font-bold'>Users Avatars</h2>
+            {users && <div>{renderImage(users)}</div>}
         </div>
     )
 }
--- services/users.js
+++ services/users.js
@@ -1,7 +1,8 @@
-const BASE_API = 'https://reqres.in/api/users?page=1'
+const BASE_API = 'https://reqres.in/api/users'
 
-export async function getUser () {
-    const res = await fetch(BASE_API)
+export async function getUser (page = 1) {
+    const url = BASE_API + `?page=${page}`
+    const res = await fetch(url)
 
     if (!res.ok) {
         throw Error(`Fetch failed: ${res}`)
